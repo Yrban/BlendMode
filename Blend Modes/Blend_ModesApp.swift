08d372f6d268461cb578d0bb619140764e4dc737
Bryan Marshall <alelinapps@gmail.com>
2021-10-10 17:05:51 -0400

    Initial Commit

---
+++ Blend Modes/Blend_ModesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blend_ModesApp.swift
+//  Blend Modes
+//
+//  Created by Developer on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Blend_ModesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
